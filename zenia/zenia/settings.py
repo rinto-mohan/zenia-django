fe4cadebb1136bc6658e2cf85433681d86f1d2d9
rinto-mohan <connectmerin2o@gmail.com>
2023-12-12 03:37:23 +0530

    modified project

--- zenia/zenia/settings.py
+++ zenia/zenia/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
-#     BASE_DIR /'zenia'/'static',
+#     BASE_DIR /'zenia'/'static',ok
 # ]
 
 
